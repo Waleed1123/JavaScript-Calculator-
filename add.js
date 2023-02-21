f2f6ce6007d3bf3d3c1b39f0821a2d5ac6e70fef
Waleed1123 <waleed007896@gmail.com>
2023-02-22 02:31:35 +0500

    calculator on progress

---
+++ add.js
@@ -0,0 +1,11 @@
+
+
+let num1 = prompt("Enter Your First Number")
+let num2 = prompt("Enter Your Second Number")
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+let num3 = num1 + num2
+
+alert ("sum is: " + num3)
